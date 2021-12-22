c2112e06994713ffe976a9496e7ebcd58e0c88bc
Hector Herrera <sumhector@gmail.com>
2021-12-22 01:02:51 -0500

    new scripts added

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
 }))
 server.use(router);
 server.listen(PORT, () => {
-  console.log('Server is running');
+  console.log('Server is running on PORT: ',PORT);
 });
