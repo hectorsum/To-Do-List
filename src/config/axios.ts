915cbb80eb093a0b3562b613f8c767269eb66c44
Hector Herrera <sumhector@gmail.com>
2021-12-22 22:12:04 -0500

    base url updated

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
+import { JSON_API } from "../helpers/Constant";
 
 const axiosClient = axios.create({
-  baseURL:'http://localhost:4000'
+  baseURL:JSON_API
 })
 
 export default axiosClient;
---
+++ src/helpers/Constant.ts
@@ -0,0 +1,1 @@
+export const JSON_API = 'https://todo-app-sum.herokuapp.com/';
